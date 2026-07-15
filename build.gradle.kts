04f5b02fdd47622a33ef874a909d345e8081d9c0
abxian <abxian@users.noreply.github.com>
2026-07-15 16:44:11 +0800

    chore: bump 2.11.43

--- build.gradle.kts
+++ build.gradle.kts
@@ -61,8 +61,8 @@ subprojects {
             minSdk = 21
             targetSdk = 35
 
-            versionName = "2.11.42"
-            versionCode = 211042
+            versionName = "2.11.43"
+            versionCode = 211043
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
